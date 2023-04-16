1c6be091bf07c712bd1a93b56addc99c06f8e37a
Kevin <scheeren@proton.me>
2023-04-16 15:25:29 +0200

    Verbesserung: Aufgabe B-1

---
+++ gerade_edit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Bitte geben Sie eine Zahl ein:\n");
+  int n;
+  scanf("%i", &n);
+  printf("Die Zahl ist %s\n", n % 2 == 0 ? "gerade" : "ungerade");
+}
